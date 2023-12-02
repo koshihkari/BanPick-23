757a22bcadca1a3d4b40f06e57328c1f272a3436
koshihkari <koshihkari37@gmail.com>
2023-12-02 20:17:02 +0900

    change main.js

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 let weapons = Array(60);
 for (let i = 0; i < 60; i++) {
     weapons[i] = 0;
-  }
-
-var count = 0;
+}
 
 function f(n) {
     if (weapons[n] == 0) {
@@ -13,5 +11,4 @@ function f(n) {
         document.getElementById(String(n)).style.visibility="hidden";
         weapons[n] = 0;
     }
-    console.log(n)
 }
